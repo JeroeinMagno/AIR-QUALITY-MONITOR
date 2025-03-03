27e483715f5ca09982a04f8e5625b5a11c56d255
Jeroein Magno <116137553+xlloyd@users.noreply.github.com>
2025-03-03 14:03:16 +0800

    adding database and updating

---
+++ sql/dml/presentation/sql/dml/presentation/0_presentation_air_quality_view.sql
@@ -0,0 +1,28 @@
+CREATE OR REPLACE VIEW presentation.air_quality AS (
+    WITH ranked_data AS (
+        SELECT
+            *,
+            ROW_NUMBER() OVER (
+                PARTITION BY location_id, sensor_id, "datetime", "parameter"
+                ORDER BY ingestion_datetime DESC
+            ) AS rn
+        FROM raw.air_quality
+        WHERE parameter IN ('pm25')
+        AND "value" >= 0
+    )
+    SELECT
+        location_id,
+        sensor_id,
+        "location",
+        "datetime",
+        lat,
+        lon,
+        "parameter",
+        units,
+        "value",
+        "month",
+        "year",
+        ingestion_datetime
+    FROM ranked_data
+    WHERE rn = 1
+);
